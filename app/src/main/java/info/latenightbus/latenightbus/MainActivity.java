f10ef6274161467c892e4714a2b6cf1d4ddce545
Paul Thillen <paul.thillen@hotmail.com>
2016-08-18 12:14:05 +0200

    Add a Fahrplan activity

--- app/src/main/java/info/latenightbus/latenightbus/MainActivity.java
+++ app/src/main/java/info/latenightbus/latenightbus/MainActivity.java
@@ -1,5 +1,7 @@
 package info.latenightbus.latenightbus;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,12 +17,15 @@ public class MainActivity extends AppCompatActivity {
         ImageButton ib_nordstad = (ImageButton) findViewById(R.id.button_nordstad);
         ImageButton ib_nordspetzt = (ImageButton) findViewById(R.id.button_nordspetzt);
         ImageButton ib_atert = (ImageButton) findViewById(R.id.button_atert);
+        final Context c = this;
         //set onClick Listeners
         ib_nordstad.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
+                        Intent intent = new Intent(c, FahrplanActivity.class);
+                        intent.putExtra("button_id",0);
+                        startActivity(intent);
                     }
                 }
         );
@@ -28,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
+                        Intent intent = new Intent(c, FahrplanActivity.class);
+                        intent.putExtra("button_id",1);
+                        startActivity(intent);
                     }
                 }
         );
@@ -36,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
+                        Intent intent = new Intent(c, FahrplanActivity.class);
+                        intent.putExtra("button_id",2);
+                        startActivity(intent);
                     }
                 }
         );
