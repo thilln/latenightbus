4f08e9c58d14ff300c734ac47fa6cce3abe3055d
Paul Thillen <paul.thillen@hotmail.com>
2016-08-18 11:45:51 +0200

    Set onClick Listeners for the 3 ImageButtons

--- app/src/main/java/info/latenightbus/latenightbus/MainActivity.java
+++ app/src/main/java/info/latenightbus/latenightbus/MainActivity.java
@@ -2,6 +2,8 @@ package info.latenightbus.latenightbus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //declare and initialize buttons
+        ImageButton ib_nordstad = (ImageButton) findViewById(R.id.button_nordstad);
+        ImageButton ib_nordspetzt = (ImageButton) findViewById(R.id.button_nordspetzt);
+        ImageButton ib_atert = (ImageButton) findViewById(R.id.button_atert);
+        //set onClick Listeners
+        ib_nordstad.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
+        ib_nordspetzt.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
+        ib_atert.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
+
     }
 }
