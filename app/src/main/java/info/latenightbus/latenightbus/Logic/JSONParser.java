a2db0d629e6b9c1a852d489913545d0d893fc696
Paul Thillen <paul.thillen@hotmail.com>
2016-09-08 22:45:11 +0200

    Created JSONParser which returns a JSONObject for a given URL

---
+++ app/src/main/java/info/latenightbus/latenightbus/Logic/JSONParser.java
@@ -0,0 +1,69 @@
+package info.latenightbus.latenightbus.Logic;
+
+/**
+ * Created by Paul Thillen on 08.09.2016.
+ */
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class JSONParser {
+
+    static InputStream is = null;
+    static JSONObject jObj = null;
+    static String json = "";
+
+    // constructor
+    public JSONParser() {
+
+    }
+
+    public JSONObject getJSONFromUrl(String url) {
+        // Making HTTP request
+        URL mUrl = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            mUrl = new URL(url);
+            urlConnection = (HttpURLConnection) mUrl.openConnection();
+            is = new BufferedInputStream(urlConnection.getInputStream());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+            jObj = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jObj;
+
+    }
+}
